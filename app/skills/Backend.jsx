c328270da1517a5d2cc83b2c79326df6c3083b4f
Abdul R <abdul.kasmo.iv@outlook.com>
2023-05-21 19:13:01 -0500

    installed basic dependencies, and create initial pages and components

---
+++ app/skills/Backend.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+function Backend() {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Development</h3>
+      <div className="skills__box">
+        <div className="skills_group">
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Node JS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Express JS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Mongodb</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Python</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Firebase</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">RESTful API</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Tailwind CSS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Backend;
---
+++ app/skills/Frontend.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function Frontend() {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Frontend Development</h3>
+      <div className="skills__box">
+        <div className="skills_group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">HTML</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">CSS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Javascript</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">React JS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Next JS</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Git</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Tailwind CSS</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Frontend;
